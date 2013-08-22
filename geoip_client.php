0e7690f9c530248591da09def8ce3366945fe86a
gooof <info@phpbb8.de>
2013-08-22 05:56:44 +0200

    Update geoip_client.php

--- geoip_client.php
+++ geoip_client.php
@@ -2,22 +2,13 @@
 <?
 
 /**
- * dsfdsf
- *
- * 
- * 
- * 
- * 
- * 
- * 
-function geoip_redis($r, $ip, $country_only = false)
+ * GEO IP function
+ * @return array
+ */
+function geoip_redis($r, $ip, $return_country_string = false)
 {
 	$ipnum = ip2long($ip);
-
-	if (!$ipnum)
-	{
-		return false;
-	}
+	if (!$ipnum) { return false; }
 	$ipnum = sprintf('%u', $ipnum);
 	
 	$res = $r->zrangebyscore(
@@ -36,26 +27,26 @@ function geoip_redis($r, $ip, $country_only = false)
 
 	list($id, $junk, $start_end) = explode(':', $k);
 
-	if ($start_end == 's')
-		if ($score > $ipnum)
-		{
-			// We have begin of new block and IP actually is not found
-			return false;
-		}
-		
+	if ($start_end == 's' && $score > $ipnum)
+	{
+		// We have begin of new block and IP actually is not found
+		return false;
+	}
+
 	$key = 'geoip:' . $id;
 	$data = $r->hgetall($key);
 
-	if ($country_only)
+	if ($return_country_string)
 	{
 		return strtoupper($data['code']);
 	}
+
 	return $data;
 }
 
 
 $r = new Redis();
 $r->connect("localhost");
-print_r( geoip_redis($r, "194.145.63.0", false) );
+print_r( geoip_redis($r, '194.145.63.0') );
 
 		
